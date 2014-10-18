0f3cc6dd4520885c38b3b2197690058f0a9573e0
Luming <lumingnie@gmail.com>
2014-10-18 15:24:05 -0400

    Added function

--- crpto_project_part_one.cpp
+++ crpto_project_part_one.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+string decrypt(stirng ctext, int t)
+{
+	// 1. x>y
+	// 2. x<y
+	// 3. x==y
+
+    vector<int> vec_t(t, 0); // vector for storing the key
+    int ctext_size = ctext.size();
+    string ptext (ctext_size, 0); // initialize the ptext string
+    for (int j = 1; j <= 40; ++j)
+    {
+        for (int i=0; i<ctext_size; i++)
+        {
+            ptext[i] = ciphertext[i] - (i/j)%t;
+        }
+        // compare the string ptext with input original message here? (by simply using "==" operator of string)
+    }
+	
+}
+
+
 int main(int argc, char const *argv[])
 {
-    ifstream cyphertext ( argv[1] );
+    ifstream ciphertext ( argv[1] );
+    ifstream t ( argv[2] );
 
-    if (!cyphertext.is_open())
+    string result;
+
+    if (!ciphertext.is_open())
     	cout<<"Could not open file\n";
     else
     {
-    	string cypher_string;
-    	while ( cyphertext >> cypher_string )
-        	cout<< cypher_string<< endl;
+        // Read input from the phertext
+    	string cipher_string;
+    	while ( ciphertext >> cipher_string )
+        	cout<< cipher_string<< endl;
+        result = decrypt( cipher_string, 5 );
     }
 
+     
 	
 	return 0;
 }
